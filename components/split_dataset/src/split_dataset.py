1c89bf845f77fdc586a01f9400a795b36a4e6997
Mike Bernico <mikebernico@google.com>
2020-11-25 17:58:48 -0600

    updated split_dataset for dated versions.

--- components/split_dataset/src/split_dataset.py
+++ components/split_dataset/src/split_dataset.py
@@ -7,9 +7,11 @@
 # ==============================================================================
 
 """Script to split titanic dataset into train/val"""
-
 import argparse
+import datetime
+import json
 import logging
+import os
 import pandas as pd
 import sklearn.model_selection
 
@@ -18,24 +20,49 @@ def parse_args():
   parser = argparse.ArgumentParser(
       description='Split Dataset Titanic KFP Component')
   parser.add_argument('--input_data', type=str, required=True,
-      help='GCS Path to input dataset.')
-  parser.add_argument('--train_output', type=str, required=True,
-      help="GCS Path to training output dataset.")
-  parser.add_argument('--val_output', type=str, required=True,
-      help="GCS Path to training output dataset.")
+      help='GCS Path to input dataset, filename included')
+  parser.add_argument('--output_path', type=str, required=True,
+      help="GCS Path to output train and val files.")
   parser.add_argument('--log_level', type=str, default="INFO",
       help="Logging level.")
   return parser.parse_args()
 
-def main():
+def write_file(path: str, content: str):
+  """Write string into local file.
+  Args:
+    path: the local path to the file.
+    content: the string content to dump.
+  """
+  directory = os.path.dirname(path)
+  if not os.path.exists(directory):
+    os.makedirs(directory)
+  elif os.path.exists(path):
+    logging.warning('The file %s will be overwritten.', path)
+  with open(path, 'w') as f:
+    f.write(content)
+
+def get_output_filenames(output_path: str):
+  """Returns a dict of output filenames."""
+  now = datetime.datetime.now()
+  now_string = now.strftime("%Y%m%d_%H%M%S")
+  filenames ={
+      'train': os.path.join(output_path, "train_split_"+now_string+".csv"),
+      'val': os.path.join(output_path, "val_split_"+now_string+".csv")
+  }
+  write_file("/tmp/train.txt", filenames['train'])
+  write_file("/tmp/val.txt", filenames['val'])
+  return filenames
+
+def split_data(cl_args: argparse.ArgumentParser):
   """Entrypoint."""
-  args = parse_args()
-  logging.getLogger().setLevel(args.log_level)
-  df = pd.read_csv(args.input_data, index_col=0)
+  filenames = get_output_filenames(cl_args.output_path)
+  df = pd.read_csv(cl_args.input_data, index_col=0)
   train, val = sklearn.model_selection.train_test_split(df, test_size=0.2,
       stratify=df['Survived'])
-  train.to_csv(args.train_output)
-  val.to_csv(args.val_output)
+  train.to_csv(filenames['train'])
+  val.to_csv(filenames['val'])
 
 if __name__== "__main__":
-  main()
+  args = parse_args()
+  logging.getLogger().setLevel(args.log_level)
+  split_data(args)
